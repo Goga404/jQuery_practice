6f6df1a5b9b07fdca4161b638de23fa530da3b3b
Goga404 <justanothergoga@gmail.com>
2017-07-04 00:29:05 +0300

    +clear input field

--- input.js
+++ input.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $("#button").click(function() {
             var toAdd = $("input[name = checkListItem]").val();  
             $(".list").append('<div class="item">' + toAdd + '</div>');
+            $("input[name = checkListItem]").val(" ");
         });
         
     $(document).on("click", ".item", function() {
